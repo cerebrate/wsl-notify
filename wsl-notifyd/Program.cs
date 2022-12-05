b4aef5d48578b0fb7a2ff03bdbf858d94994ddd6
Alistair Young <avatar@arkane-systems.net>
2022-12-05 12:17:18 -0600

    Socket experiment #1.

--- wsl-notifyd/Program.cs
+++ wsl-notifyd/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 using Tmds.DBus;
@@ -11,14 +12,23 @@ namespace ArkaneSystems.Wsl.Notify
     {
         public static async Task Main(string[] args)
         {
-            var connection = new Connection(Address.Session!);
-            await connection.ConnectAsync();
-            await connection.RegisterObjectAsync(new WslNotifier());
-            await connection.RegisterServiceAsync ("org.freedesktop.Notifications");
+            // var connection = new Connection(Address.Session!);
+            // await connection.ConnectAsync();
+            // await connection.RegisterObjectAsync(new WslNotifier());
+            // await connection.RegisterServiceAsync ("org.freedesktop.Notifications");
 
-            Console.WriteLine ("Monitoring for notifications. Press CTRL-C to stop.");
+            // Console.WriteLine ("Monitoring for notifications. Press CTRL-C to stop.");
 
-            await Task.Delay(int.MaxValue);
+            // await Task.Delay(int.MaxValue);
+
+            // Write to windows socket.
+            var unixSocketPath = "/mnt/c/Users/avatar/wslnote.sock";
+
+            var socket = new Socket (AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
+            var unixEp = new UnixDomainSocketEndPoint(unixSocketPath);
+            socket.Connect (unixEp);
+
+            socket.Close();
         }
     }
 }
